30cca0fab433b6926fd4cd927968d5d5df380444
aLaskevic <al.laskevic@gmail.com>
2023-01-24 18:55:02 +0100

    feature: Added User Icons and modal

--- src/components/UserIcon/UserIcon.jsx
+++ src/components/UserIcon/UserIcon.jsx
@@ -1,37 +1,87 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./UserIcon.css";
 
 export default function UserIcon(props) {
   const [isOption, setIsOption] = useState(false);
-  console.log(props.connection);
-  console.log(props.user);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div
-      className="user"
-      onMouseEnter={() => setIsOption(true)}
-      onMouseLeave={() => setIsOption(false)}
-    >
-      {isOption ? (
-        <div className="user-menu">
-          {props.connection.sessionId == props.connection.userId ? (
-            <FontAwesomeIcon className="kick" icon={faXmark}></FontAwesomeIcon>
-          ) : (
-            ""
-          )}
-          <FontAwesomeIcon
-            className="user-menu-entry"
-            icon={faVolumeXmark}
-          ></FontAwesomeIcon>
+    <>
+      <div
+        className="user"
+        onMouseEnter={() => setIsOption(true)}
+        onMouseLeave={() => setIsOption(false)}
+      >
+        {isOption ? (
+          <div className="user-menu">
+            {props.connection.sessionId == props.connection.userId ? (
+              <FontAwesomeIcon
+                className="kick"
+                icon={faXmark}
+                onClick={() => setIsModalOpen(true)}
+              ></FontAwesomeIcon>
+            ) : (
+              ""
+            )}
+            {isMuted ? (
+              <FontAwesomeIcon
+                className="user-menu-entry"
+                icon={faVolumeXmark}
+                onClick={() => {
+                  setIsMuted((prev) => !prev);
+                }}
+              ></FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon
+                className="user-menu-entry"
+                icon={faVolumeHigh}
+                onClick={() => {
+                  setIsMuted((prev) => !prev);
+                }}
+              ></FontAwesomeIcon>
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+        <div
+          className="user-icon"
+          style={{ backgroundColor: props.user.color }}
+        >
+          {props.user.name.at(0).toUpperCase()}
+        </div>
+      </div>
+      {isModalOpen ? (
+        <div className="kick-modal">
+          <div className="kick-modal-content">
+            <h3>
+              Are you sure that you want to kick{" "}
+              <span className="kick-modal-content-user">{props.user.name}</span>
+              ?
+            </h3>
+            <div className="kick-modal-buttons">
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="kick-modal-buttons-kick"
+              >
+                Kick
+              </button>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="kick-modal-buttons-cancel"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
         </div>
       ) : (
         ""
       )}
-      <div className="user-icon" style={{ backgroundColor: props.user.color }}>
-        {props.user.name.at(0).toUpperCase()}
-      </div>
-    </div>
+    </>
   );
 }
